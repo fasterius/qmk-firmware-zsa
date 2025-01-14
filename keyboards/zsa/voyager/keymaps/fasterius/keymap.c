7f401d219618d46114a227f6d825d6ccc7e5371d
Erik Fasterius <erik.fasterius@outlook.com>
2025-01-14 14:58:18 +0100

    Rename `RCTL_CLON` to `RCTL_COLN`

--- keyboards/zsa/voyager/keymaps/fasterius/keymap.c
+++ keyboards/zsa/voyager/keymaps/fasterius/keymap.c
@@ -32,7 +32,7 @@ enum tap_dance_codes {
 #define RSFT_J MT(MOD_RSFT, KC_J)
 #define RGUI_K MT(MOD_RGUI, KC_K)
 #define RALT_L MT(MOD_RALT, KC_L)
-#define RCTL_CLON TD(DANCE_0)
+#define RCTL_COLN TD(DANCE_0)
 #define L2_SPACE LT(2, KC_SPACE)
 #define MEH_TAB MEH_T(KC_TAB)
 #define MEH_BSPC MEH_T(KC_BSPC)
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
     KC_EQUAL , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,           KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_MINUS ,
     KC_DEL   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,           KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_BSLS  ,
-    KC_ESC   , LCTL_A   , LALT_S   , LGUI_D   , LSFT_F   , KC_G     ,           KC_H     , RSFT_J   , RGUI_K   , RALT_L   , RCTL_CLON, KC_QUOTE ,
+    KC_ESC   , LCTL_A   , LALT_S   , LGUI_D   , LSFT_F   , KC_G     ,           KC_H     , RSFT_J   , RGUI_K   , RALT_L   , RCTL_COLN, KC_QUOTE ,
     KC_CAPS  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,           KC_N     , KC_M     , KC_COMMA , KC_DOT   , KC_SLASH , CW_TOGG  ,
                                                L2_SPACE  , MEH_TAB  ,           MEH_BSPC , L1_ENTER
   ),
