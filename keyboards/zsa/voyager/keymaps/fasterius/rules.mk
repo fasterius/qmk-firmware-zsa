36a63be95d429274b7194602c9cafd23c149a7cd
Erik Fasterius <erik.fasterius@outlook.com>
2025-01-15 09:07:54 +0100

    Fix custom shift keys; remove commented code
    Fix the custom shift keys using custom code in `process_record_user`
    instead of `custom_shift_keys` or key overrides.

--- keyboards/zsa/voyager/keymaps/fasterius/rules.mk
+++ keyboards/zsa/voyager/keymaps/fasterius/rules.mk
@@ -1,5 +1,3 @@
-# Set any rules.mk overrides for your specific keymap here.
-# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 CAPS_WORD_ENABLE = yes
 COMBO_ENABLE = yes
 COMMAND_ENABLE = no
