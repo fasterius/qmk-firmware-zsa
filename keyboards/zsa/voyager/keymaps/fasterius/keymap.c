bbcc1a304a2bcfb1fa938d0690733e8d82108360
Erik Fasterius <erik.fasterius@outlook.com>
2025-01-16 11:31:07 +0100

    Use `KC_SYSTEM_SLEEP` to sleep on both Win/Mac

--- keyboards/zsa/voyager/keymaps/fasterius/keymap.c
+++ keyboards/zsa/voyager/keymaps/fasterius/keymap.c
@@ -62,6 +62,7 @@ enum custom_layers {
 #define LALT_PREV MT(MOD_LALT, KC_MEDIA_PREV_TRACK)
 #define LGUI_NEXT MT(MOD_LGUI, KC_MEDIA_NEXT_TRACK)
 #define LSFT_PLAY MT(MOD_LSFT, KC_MEDIA_PLAY_PAUSE)
+#define SLEEP KC_SYSTEM_SLEEP
 
 // Gaming layer aliases
 #define ALT_TAB MT(MOD_LALT, KC_TAB)
@@ -92,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX  , LED_LEVEL, RGB_VAD  , RGB_VAI  , RGB_TOG  , XXXXXXX  ,           XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , QK_BOOT  ,
     XXXXXXX  , XXXXXXX  , VOL_DOWN , VOL_UP   , MUTE     , XXXXXXX  ,           KC_HOME  , KC_PGDN  , PAGE_UP  , KC_END   , XXXXXXX  , MAC_AA   ,
     SEL_ALL  , LCTL_STOP, LALT_PREV, LGUI_NEXT, LSFT_PLAY, TOGL_LANG,           KC_LEFT  , KC_DOWN  , KC_UP    , KC_RIGHT , MAC_OSLH , MAC_ADIA ,
-    REDO     , UNDO     , CUT      , COPY     , PASTE    , KC_F19   ,           XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
+    REDO     , UNDO     , CUT      , COPY     , PASTE    , SLEEP    ,           XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
                                                 _______  , _______  ,           _______  , _______
   ),
 
